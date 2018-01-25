1dfa86b98ef738173166b8cc88d7c5736a954ddb
Sabeena N Singhani <sabeena@nyu.edu>
2018-01-25 03:48:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function shout(string) { return string.toUpperCase () }
-function whisper(string) { return string.toLowerCase () }
-function sayHiToGrandma(string) { var lowercase = 'hello!' if (lowercase.toLowerCase() === lowercase) {return "I can't hear you!"} } 
-console.log(Shout) (`The header is: ${Shout}`)
-console.log(Whisper) 
+function shout(string)
+{
+return
+string.toUpperCase ()
+}
